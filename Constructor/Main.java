c07e21636e6ce33f1d9cfccce9ff87bcd746d038
Mahbub Morshed <45009800+rishatmorshed@users.noreply.github.com>
2022-11-18 03:02:55 +0600

    Add files via upload

---
+++ Constructor/Main.java
@@ -0,0 +1,11 @@
+package Encapsulation;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        student ob1 = new student("Morshed", 2114); // set value during creat an object(called constructor);
+        student ob2 = new student("Rishat", 211403);
+        ob1.display();
+        ob2.display();
+    }
+}
---
+++ Constructor/student.java
@@ -0,0 +1,14 @@
+package Encapsulation;
+
+public class student {
+    String name;
+    int id;
+    student(String n, int i){
+        this.name = n;
+        this.id = i;
+    }
+    void display(){
+        System.out.println("Name: "+name);
+        System.out.println("ID: "+id);
+    }
+}
