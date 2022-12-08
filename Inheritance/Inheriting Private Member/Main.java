aff73b9835be897cb5738cda16c2e2c8ebe8a18f
Mahbub Morshed <45009800+rishatmorshed@users.noreply.github.com>
2022-12-08 19:58:33 +0600

    Update Main.java

--- Inheritance/Inheriting Private Member/Main.java
+++ Inheritance/Inheriting Private Member/Main.java
@@ -2,7 +2,6 @@ package Mahbub_Morshed;
 
 public class Main {
     public static void main(String[] args) {
-        person p = new person();
         student s = new student();
         s.input4person();
         s.input4student();
